1db37061282cf4837dc00f98708bb7b8f20d8b57
DMotorina <lady.renfri@gmail.com>
2024-03-08 11:54:35 +0300

    Поправила верстку

--- frontend/src/features/login/Login.jsx
+++ frontend/src/features/login/Login.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import {  Link } from 'react-router-dom';
+
+import { Row, Container, Col, Card, Image } from 'react-bootstrap';
+
 import { LoginForm } from "./LoginForm"
+
+import logo from '../../assets/login-image.jpeg'
 
 export const Login = () => {
     return (
-        <div className="loginPage">
-            <LoginForm />
-        </div>
+        <Container fluid className="h-100">
+            <Row className="justify-content-center align-content-center h-100">
+            <Col className="col-12 col-md-8 col-xxl-6">
+                <Card className="shadow-sm">
+                <Card.Body className="row p-5">
+                    <Col className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                    <Image src={logo} roundedCircle alt="Войти" />
+                    </Col>
+                    <LoginForm />
+                </Card.Body>
+                <Card.Footer className="p-4">
+                    <div className="text-center">
+                    <span>Нет аккаунта? </span>
+                    <Link to="/signup">Регистрация</Link>
+                    </div>
+                </Card.Footer>
+                </Card>
+            </Col>
+            </Row>
+        </Container>
     )
 }
--- frontend/src/features/login/LoginForm.jsx
+++ frontend/src/features/login/LoginForm.jsx
@@ -1,37 +1,13 @@
-import React, {useState, useRef, useEffect } from "react";
-import { useFormik, Formik, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import axios from 'axios';
-import { useLocation, useNavigate, Link } from 'react-router-dom';
-import { setCredentials } from '../../slices/userSlice';
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import {
-  Row,
-  Container,
-  Button,
-  Navbar,
-  Col,
-  Card,
-  Image,
-  Form,
-} from 'react-bootstrap';
-import logo from '../../assets/login-image.jpeg'
+import { Form, Button } from 'react-bootstrap';
 
+import { useFormik } from "formik";
 
-const SignupSchema = Yup.object().shape({
-  username: Yup
-    .string()
-    .min(2, 'Минимум 2 буквы')
-    .max(10, 'Максимум 10 букв')
-    .required('Обязательное поле'),
-  
-  password: Yup
-    .string()
-    .min(2, 'Минимум 2 буквы')
-    .max(10, 'Максимум 10 букв')
-    .required('Обязательное поле')
-});
+import axios from 'axios';
 
+import { setCredentials } from '../../slices/userSlice';
 
 export const LoginForm = () => {
   const inputUsernameElem = useRef(null);
@@ -63,60 +39,39 @@ export const LoginForm = () => {
   }})
 
   return (
-    <div className="d-flex justify-content-center align-items-center">
-      <Container fluid className="h-100">
-        <Row className="justify-content-center align-content-center h-100">
-          <Col className="col-12 col-md-8 col-xxl-6">
-            <Card className="shadow-sm">
-              <Card.Body className="row p-5">
-                <Col className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                  <Image src={logo} roundedCircle alt="Войти" />
-                </Col>
-                <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
-                  <h1 className="text-center mb-4">Войти</h1>
-                  <Form.Floating className="mb-3">
-                    <Form.Control
-                      isInvalid={validated}
-                      name="username"
-                      id="username"
-                      autoComplete="username"
-                      required
-                      type="text"
-                      placeholder="Ваш ник"
-                      value={formik.values.username}
-                      onChange={formik.handleChange}
-                      ref={inputUsernameElem}
-                    />
-                    <Form.Label htmlFor="username">Ваш ник</Form.Label>
-                  </Form.Floating>
-                  <Form.Floating className="mb-3">
-                    <Form.Control
-                      isInvalid={validated}
-                      name="password"
-                      id="password"
-                      autoComplete="current-password"
-                      required
-                      type="password"
-                      placeholder="Пароль"
-                      value={formik.values.password}
-                      onChange={formik.handleChange}
-                    />
-                    <Form.Label htmlFor="password">Пароль</Form.Label>
-                  </Form.Floating>
-                  <div className="invalid-tooltip">Неверные имя пользователя или пароль</div>
-                  <Button disabled={formik.isSubmitting} variant="outline-primary" type="submit" className="w-100 mb-3">Войти</Button>
-                </Form>
-              </Card.Body>
-              <Card.Footer className="p-4">
-                <div className="text-center">
-                  <span>Нет аккаунта? </span>
-                  <Link to="/signup">Регистрация</Link>
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
+      <h1 className="text-center mb-4">Войти</h1>
+      <Form.Floating className="mb-3">
+        <Form.Control
+          isInvalid={validated}
+          name="username"
+          id="username"
+          autoComplete="username"
+          required
+          type="text"
+          placeholder="Ваш ник"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          ref={inputUsernameElem}
+        />
+        <Form.Label htmlFor="username">Ваш ник</Form.Label>
+      </Form.Floating>
+      <Form.Floating className="mb-3">
+        <Form.Control
+          isInvalid={validated}
+          name="password"
+          id="password"
+          autoComplete="current-password"
+          required
+          type="password"
+          placeholder="Пароль"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <Form.Label htmlFor="password">Пароль</Form.Label>
+        <div className="invalid-tooltip">Неверные имя пользователя или пароль</div>
+      </Form.Floating>
+      <Button disabled={formik.isSubmitting} variant="outline-primary" type="submit" className="w-100 mb-3">Войти</Button>
+    </Form>
   );
 };
