7e19a8a01127c06ab479b475ea34255ab0f93075
DMotorina <lady.renfri@gmail.com>
2024-03-22 10:03:03 +0300

    Завершение 7-го шага

--- frontend/src/features/header/Header.jsx
+++ frontend/src/features/header/Header.jsx
@@ -1,18 +1,30 @@
 import { Container, Button, Navbar } from 'react-bootstrap';
   
 import { Link, Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { removeCredentials } from '../../slices/usersSlice.js';
   
 export const Header = () => {
+  const token = useSelector((state) => state.users.token);
+
+  const dispatch = useDispatch();
+
+  const handeleRemoveUser = () => {
+    dispatch(removeCredentials());
+    localStorage.removeItem('userId');
+  };
+
     return (
         <div className="d-flex flex-column h-100">
         <Navbar className="shadow-sm" bg="white" expand="lg">
           <Container>
             <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-            <Link to="/login"><Button>Выйти</Button></Link>
+            {token ? <Button onClick={() => handeleRemoveUser()} as={Link} to="/login">Выйти</Button>
+            : null}
           </Container>
         </Navbar>
         <Outlet />
       </div>
     )
 }
-  
