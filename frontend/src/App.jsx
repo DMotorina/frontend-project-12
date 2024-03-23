8d9158ce3eb1510d842f8d8a609055f3c2133616
DMotorina <lady.renfri@gmail.com>
2024-03-23 10:20:30 +0300

    Настройка i18n

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { AppRoutes } from './AppRoutes'
 import { Provider } from 'react-redux'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import initI18next from './initI18next';
 
+export default async () => {
+  await initI18next();
 
-export const App = () => {
   return (
     <Provider store={store}>
       <AppRoutes />
