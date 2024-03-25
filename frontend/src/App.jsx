b5ca7d32c7ecf3e9783ac5e216a2ef56592519c4
DMotorina <lady.renfri@gmail.com>
2024-03-25 09:54:40 +0300

    Подключение rollbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import initI18next from './initI18next';
 import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 export default async () => {
   await initI18next();
@@ -11,9 +12,22 @@ export default async () => {
   const ruDict = leoProfanity.getDictionary('ru');
   leoProfanity.add(ruDict);
 
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+    payload: {
+      environment: 'production',
+    },
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  };
+
   return (
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <AppRoutes />
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   )
 }
