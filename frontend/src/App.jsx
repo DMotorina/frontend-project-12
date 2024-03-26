598fea25e6e9ce39cc728fbcbc52d3973429091c
DMotorina <lady.renfri@gmail.com>
2024-03-26 11:23:29 +0300

    Настройка rollbarConfig

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,13 @@ import initI18next from './initI18next';
 import leoProfanity from 'leo-profanity';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
+import rollbarConfig from './rollbar/rollbarConfig';
+
 export default async () => {
   await initI18next();
 
   const ruDict = leoProfanity.getDictionary('ru');
   leoProfanity.add(ruDict);
-
-  const rollbarConfig = {
-    accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    payload: {
-      environment: 'production',
-    },
-  };
 
   return (
     <RollbarProvider config={rollbarConfig}>
